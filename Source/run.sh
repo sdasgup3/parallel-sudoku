039ffd08bfa26358b453085914fbcadc35b2b754
Sandeep Dasgupta <sdasgup3@illinois.edu>
2014-11-30 22:04:23 -0600

    modified to support regr run

--- Source/run.sh
+++ Source/run.sh
@@ -6,5 +6,25 @@ TARGET=gc
 # Enter the command line parameters here. --help to get a list of allowed
 # options
 PROGRAM_OPTIONS=--newGraph=yes
-set -x
+#set -x
+runreg=false
+
+while getopts ":r" opt; do
+  case $opt in
+    r)
+      for file in $( find ../Tests/ -name "*json" ); do
+        echo $file
+        cp $file latestGraph.json
+        ./charmrun +p4 $TARGET --newGraph=no ++local
+      done
+      runreg=true  
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+if [ $runreg = "false" ] ; then
 ./charmrun +p4 $TARGET $PROGRAM_OPTIONS ++local
+fi
