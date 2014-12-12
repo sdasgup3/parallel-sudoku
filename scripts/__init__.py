e3218d9c7d2ebd04143a8adea75811fc630105ce
Sandeep Dasgupta <sdasgup3@illinois.edu>
2014-12-12 03:25:39 -0600

    making nornal graph as default

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-import subgraphGenClass
-#import graphGenClass
+#import subgraphGenClass
+import graphGenClass
 
 def getGraph(incoming):
-  obj = subgraphGenClass.subgraphGen()  # create an instance of graphGenClass
-  #obj = graphGenClass.graphGen()  # create an instance of graphGenClass
+  #obj = subgraphGenClass.subgraphGen()  # create an instance of graphGenClass
+  obj = graphGenClass.graphGen()  # create an instance of graphGenClass
   return obj.generate(incoming)  # return data to MainChare!
